2c7a26c6fe8da861f019acd2b8354252c146f497
Kevin Klein <k.klein@intershop.de>
2021-04-08 23:10:11 +0200

    extend contract and replace metadata file

--- contracts/DynamicNft.sol
+++ contracts/DynamicNft.sol
@@ -19,6 +19,16 @@ contract DynamicNft is ERC721, VRFConsumerBase, Ownable {
     string requestToSender;
     string requestToTokenId;
 
+    struct Spaceship {
+        uint8 width;
+        uint8 height;
+        string createdAt;
+        string format;
+        string url;
+    }
+
+    Spaceship public spaceship;
+
     constructor(address _VRFCoordinator, address _LINKToken, uint256 _keyhash) 
     public
     VRFCoordinator(_VRFCoordinator, _LINKToken)
@@ -28,5 +38,35 @@ contract DynamicNft is ERC721, VRFConsumerBase, Ownable {
         LINKToken = _LINKToken;
         keyhash = _keyhash;
         fee = 0.1 * 10**18;
+    }
+
+    function getSpaceshipOverview() public returns ( uint8, uint8, string, string, string){
+        return (
+            spaceship.width,
+            spaceship.height,
+            spaceship.createdAt,
+            spaceship.format,
+            spaceship.url
+        );
+    }
+
+    function getWidth() public returns (int8){
+        return spaceship.width;
+    }
+
+    function getHeight() public returns (int8){
+        return spaceship.height;
+    }
+
+    function getCreationTime() public returns (int8){
+        return spaceship.createdAt;
+    }
+
+    function getFormat() public returns (int8){
+        return spaceship.format;
+    }
+
+    function getSourcUrl() public returns (int8){
+        return spaceship.url;
     }
 }
